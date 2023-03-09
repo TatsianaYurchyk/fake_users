2325b5bbb697ef00e47da621b2ade6a8798ad548
TatsianaYurchik <t26yurchik@gmail.com>
2023-03-09 10:55:29 +0300

    add seed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import { faker } from '@faker-js/faker';
 import UserTable from './Table';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Context } from "./Context.js";
+import { useState} from "react";
 
 
 function App() {
+  const [value, setValue] = useState("0");
   return (
+    <Context.Provider value={[value, setValue]}>
     <Container>
      <UserTable/>
      </Container>
+     </Context.Provider>
   );
 }
 
--- src/Table.jsx
+++ src/Table.jsx
@@ -1,14 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useState, useContext  } from "react";
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
-
+import { Context } from "./Context";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import { DataGrid, GridColTypeDef, GridRowId } from "@mui/x-data-grid";
+import { DataGrid } from "@mui/x-data-grid";
 import { faker } from "@faker-js/faker";
+import SliderRange from "./SliderRange"
+import Form from 'react-bootstrap/Form';
 
 const UserTable = () => {
 	const [users, setUsers] = useState([]);
 	const [pageSize, setPageSize] = useState(10);
+	const [seed, setSeed] = useState("");
+	const [value, setValue] = useContext(Context);
 
 	function User(fullName, id, phone, address) {
 		this.fullName = fullName;
@@ -16,27 +20,52 @@ const UserTable = () => {
 		this.phone = phone;
 		this.address = address;
 	}
+	//for future:simple creation
+
+	// const create = () => {
+	//	faker.seed(+seed);
+	// 	let fullName = faker.name.fullName();
+	// 	let phone = faker.phone.number();
+	// 	let id = faker.datatype.uuid();
+	// 	let address =
+	// 		faker.address.street() +
+	// 		" " +
+	// 		faker.address.buildingNumber() +
+	// 		" " +
+	// 		faker.address.city();
+	// 	let user = new User(fullName, id, phone, address);
+	// 	let newUsers = users;
+	// 	newUsers = [...users, user];
+	// 	setUsers(newUsers);
+	// };
 
 	const create = () => {
-		let fullName = faker.name.fullName();
-		let phone = faker.phone.number();
-		let id = faker.datatype.uuid();
-		let address =
-			faker.address.street() +
-			" " +
-			faker.address.buildingNumber() +
-			" " +
-			faker.address.city();
-		let user = new User(fullName, id, phone, address);
-		let newUsers = users;
-		newUsers = [...users, user];
-		setUsers(newUsers);
+		let userrss = [];
+     	faker.seed(+seed);
+		for (let i = 0; i < value; i++) {
+		  let fullName = faker.name.fullName();
+		  let phone = faker.phone.number();
+		  let id = faker.datatype.uuid();
+		  let address =
+			  faker.address.street() +
+			  " " +
+			  faker.address.buildingNumber() +
+			  " " +
+			  faker.address.city();
+		  let user = new User(fullName, id, phone, address);
+		  userrss.push(user)
+	  };
+	  let newUsers = users;
+	  let finalUsers=newUsers.concat(userrss)
+		  setUsers(finalUsers);
+		  console.log(finalUsers)
 	};
+
 
 	const createHe = () => {
 		faker.locale = "he";
 		create();
-	};
+}
 	const createDe = () => {
 		faker.locale = "de";
 		create();
@@ -45,7 +74,10 @@ const UserTable = () => {
 	const createRu = () => {
 		faker.locale = "ru";
 		create();
+		
 	};
+
+	
 
 	const columns = [
 		{ field: "id", headerName: "id", width: 250 },
@@ -70,13 +102,14 @@ const UserTable = () => {
 					<Button variant="secondary" onClick={createRu}>
 						Russian
 					</Button>
+              <Form.Control onChange={e => setSeed(e.target.value)} placeholder="Seed" />
 				</ButtonGroup>
+				<SliderRange/>
 				<div style={{ height: 600, width: "100%" }}>
 					<DataGrid
 						className="table"
 						rows={users}
 						columns={columns}
-						// checkboxSelection
 						disableSelectionOnClick
 						getRowId={(row) => row.id}
 						pageSize={pageSize}
